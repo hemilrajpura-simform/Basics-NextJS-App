ff8cd62059ca927d7217a9633ace3041f0353a41
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-09-14 16:23:20 +0530

    worked on fetching data using SSR

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images:{
+//     domains:["https://jsonplaceholder.typicode.com"],
+//     formats:["image/webp"],
+//   }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ["via.placeholder.com"],
+    // formats: ["image/webp"],
+  }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data)
   return (
     <>
-    Initial Structure
+  {data.map((item)=>(
+    <>
+    <p>{item.title}</p>
+    <Image src={item.url} height={200} width={200} alt={item.title} />
+    </>
+  ))}
     </>
   )
 }
-  
+
+export async function getServerSideProps(){
+    // Fetch data from external API
+    const res = await fetch
+    (`https://jsonplaceholder.typicode.com/photos`)
+    const data = await res.json();
+
+    return{
+      props:{data:data}
+    }
+}
